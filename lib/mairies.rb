a0fbda26719fbf03f8b80cab6081632b79c4bf16
Jeanne Colmou <jeannecolmou@mbp-de-jeanne.home>
2021-01-22 02:39:16 +0100

    SCRAPP

--- lib/mairies.rb
+++ lib/mairies.rb
@@ -1,33 +1,41 @@
+require 'bundler'
+Bundler.require
+require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'httparty'
 
 def scrap
-        
-    url = "http://annuaire-des-mairies.com/val-d-oise.html"
-    page = Nokogiri::HTML(URI.open(url))
-    noms = page.xpath('//a[@class="lientxt"]').map{|element| element = element.text}
 
-      town_urls_array = []
+url = "http://annuaire-des-mairies.com/val-d-oise.html"
+
+#mail des maires
+     
+def get_townhall_email(url)
+	page = Nokogiri::HTML(URI.open(url))
+	names = page.xpath('//a[@class="lientxt"]').map{|element| element = element.text}
+	return email
+end
+
+
+town_urls_array = []
      
       arr_hash = []
 
-     page.each do |url|
-        town_urls_array.push(url.text.delete_prefix('.').prepend('https://www.annuaire-des-mairies.com'))
-
-    end
-
-    email = []
-
-    nb = town_urls_array.length
-
-    nb.times do |index|
-        page_mail = Nokogiri::HTML(URI.open(town_urls_array [index]))
-        emails = page_mail.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').map{|element| element = element.text}
-        puts email
-    end
-
-puts noms
-    
+page.each do |url|
+	town_urls_array.push(url.text.delete_prefix('.').prepend('https://www.annuaire-des-mairies.com'))
 end
 
-scrap
+email = []
+
+nb = town_urls_array.length
+
+nb.times do |index|
+	page_mail = Nokogiri::HTML(URI.open(town_urls_array [index]))
+	emails = page_mail.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').map{|element| element = element.text}
+        puts email #Donne l'email de chaque mairie en colonne 
+end
+
+puts names
+    
+end
--- lib/scrapp.rb
+++ lib/scrapp.rb
@@ -1,25 +1,43 @@
-def scrap
-    url = "https://coinmarketcap.com/all/views/all/"
-    page = Nokogiri::HTML(open(url))
+require 'bundler'
+Bundler.require
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'httparty'
+    
+
+url = "https://coinmarketcap.com/all/views/all/"
+page = Nokogiri::HTML(open(url))
 
 
-    arr_name = page.xpath('//*[@class="cmc-table-row"]//td[2]/div').map{|element| element = element.text}
-    arr_value = page.xpath('//*[@class="cmc-table-row"]//td[5]/div').map{|element| element = element.text}
+	 arr_name = page.xpath('//*[@class="cmc-table-row"]//td[2]/div').map{|element| element = element.text} 
+=begin
+Je recupere le Xpath des symboles, je lui demande de prendre la colonne 2 du site Coinmarket
+=end
+    	arr_value = page.xpath('//*[@class="cmc-table-row"]//td[5]/div').map{|element| element = element.text}
 
-  arr_name.each do |name|
-    hash = {}
-  end
+=begin 
+Pareil, je sors tous les liens qui ont un titre dans la classe "table row", a chaque fois il cree un array avec les donnees demandees
+=end
 
-  # crypto = {
-#   currency = row.xpath('td[2]/div/a').text
-#   value = row.xpath('td[5]/div/a').text.gsub('$', '').gsub(',', '').to_f
-# }
+=begin
+Tentative de boucle pour lire les "devices" telles que BTS, ETH... pour les classer par la suite dans un array 
+=end
 
-  hash_result[arr_name] = arr_value
-
-
-    hash_final = Hash[arr_name.zip(arr_value)]
-
-    puts hash_final
-
+def hash_together
+	arr_name.collect do |name| 
+    	hash = {}
+	crypto = {}
+	currency = row.xpath('td[2]/div/a').text
+	value = row.xpath('td[5]/div/a').text.gsub('$', '').gsub(',', '').to_f
+	end
 end
+
+def perform
+	hash_result[arr_name] = arr_value		#test1 Classer chaque devices w/ prices
+	hash_final = Hash[arr_name.zip(arr_value)]	#test2 Sortir 200 hashs dans un array
+	puts hash_final
+end
+
+
+perform
